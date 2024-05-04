2d875c186cbd5b3b3748ec0a1c9a4b04330d8041
yakutdmitry <D23125246@mytudublin.ie>
2024-05-04 01:08:14 +0100

    Dungeon is Ready

---
+++ Assets/Cake.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cake : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
